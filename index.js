696c968045f0e8017100c250f7beb1097d1d99a4
GabrielL <79658132+gluna1203@users.noreply.github.com>
2021-11-17 18:51:24 -0700

    Database connected

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const pug = require('pug');
+const expressSession = require('express-session');
+const routes = require('./routes/routes.js');
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + "/views");
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(expressSession({
+    secret: 'wh4t3v3r',
+    saveUninitialized: true,
+    resave: true
+}));
+
+const urlencodedParser = express.urlencoded({
+    extended: false
+})
+
+const checkAuth = (req, res, next) => {
+    if (req.session.user && req.session.user.isAuthenticated){
+        next();
+    } else {
+        res.redirect('/');
+    }
+}
+
+app.listen(3000);
---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+const { MongoClient, ObjectId }= require('mongodb')
+
+const url = 'mongodb+srv://ADMIN:Neumont@thehubusers.2xiyk.mongodb.net/Userbase?retryWrites=true&w=majority';
+
+const client = new MongoClient(url);
+
+const dbName = 'Userbase';
+const db = client.db(dbName);
+const collection = db.collection('Users');
