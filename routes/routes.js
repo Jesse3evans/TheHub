746f00dca02a1438b6bc11714160c55e274b2887
GabrielL <79658132+gluna1203@users.noreply.github.com>
2022-01-28 14:34:37 -0700

    y

--- routes/routes.js
+++ routes/routes.js
@@ -16,7 +16,7 @@ exports.login = (req, res) => {
 }
 
 const hashComplete = (password, the_hash) => {
-    bcrypt.compare(password, the_hash, (err, res) =>{
+    bcrypt.compare(password, the_hash, (err, res) => {
         console.log('async: ' + res);
     })
 }
@@ -24,17 +24,17 @@ const hashComplete = (password, the_hash) => {
 exports.loginAuth = async (req, res) => {
     await client.connect();
 
-    if(req.body.username == undefined || req.body.password == undefined){
+    if (req.body.username == undefined || req.body.password == undefined) {
         res.redirect('/login');
     } else {
-        const filteredDocs = await users.find({username: req.body.username}).toArray()
-        if (bcrypt.compareSync(req.body.password, filteredDocs[0].password)){
+        const filteredDocs = await users.find({ username: req.body.username }).toArray()
+        if (bcrypt.compareSync(req.body.password, filteredDocs[0].password)) {
             req.session.user = {
                 isAuthenticated: true,
                 username: filteredDocs[0].username
             }
             console.log("Username: " + req.body.username)
-            res.redirect('/feed/'+req.body.username)
+            res.redirect('/feed/' + req.body.username)
         } else {
             res.redirect('/login')
         }
@@ -55,11 +55,11 @@ exports.feed = async (req, res) => {
     await client.connect();
     const postsResults = await (await posts.find({}).toArray()).reverse();
     const usersResults = await users.find({}).toArray();
-    let rawuser ={
-        username:req.params.username
+    let rawuser = {
+        username: req.params.username
     }
     console.log("Username: " + rawuser.username);
-    const filteredDocs = await users.findOne({username: rawuser.username});
+    const filteredDocs = await users.findOne({ username: rawuser.username });
     client.close();
     res.render('feed', {
         postArray: postsResults,
@@ -96,30 +96,30 @@ exports.createUser = async (req, res) => {
 
 exports.userProfile = async (req, res) => {
     await client.connect();
-    const filteredDocs = await users.findOne({username: req.params.id});
-    let rawuser =  filteredDocs ; 
+    const filteredDocs = await users.findOne({ username: req.params.id });
+    let rawuser = filteredDocs;
     //search through array of user.friends then see what to do 
-    const postsResults = await (await posts.find({user: req.params.id}).toArray()).reverse();
+    const postsResults = await (await posts.find({ user: req.params.id }).toArray()).reverse();
     console.log(postsResults);
     console.log(rawuser);
     client.close();
     res.render('userProfile', {
         user: filteredDocs,
-        postArray: postsResults 
+        postArray: postsResults
     });
 };
 exports.otherProfile = async (req, res) => {
     await client.connect();
-    const filteredDocs = await users.findOne({username: req.params.mainUser});
-    const other = await users.findOne({username: req.params.otherUser});
+    const filteredDocs = await users.findOne({ username: req.params.mainUser });
+    const other = await users.findOne({ username: req.params.otherUser });
     //search through array of user.friends then see what to do 
-    const postsResults = await posts.find({user: req.params.otherUser}).toArray();
+    const postsResults = await posts.find({ user: req.params.otherUser }).toArray();
     console.log(postsResults);
     client.close();
     res.render('friendProfile', {
         user: filteredDocs,
         otherUser: other,
-        postArray: postsResults 
+        postArray: postsResults
     });
 };
 
@@ -136,13 +136,13 @@ exports.viewPost = async (req, res) => {
     await client.connect();
     var id = Math.floor(req.params.id);
     console.log(id)
-    const filteredDocs = await posts.findOne({postId: id});
-    const mainUser = await users.findOne({username: req.params.username});
+    const filteredDocs = await posts.findOne({ postId: id });
+    const mainUser = await users.findOne({ username: req.params.username });
     console.log(filteredDocs)
     client.close();
     res.render('post', {
         post: filteredDocs,
-        user:mainUser
+        user: mainUser
     });
 };
 
@@ -158,80 +158,84 @@ exports.createPost = async (req, res) => {
     let date = date_ob.getDate();
     let month = date_ob.getMonth() + 1;
     let year = date_ob.getFullYear();
-   // current hours
+    // current hours
     let hours = date_ob.getHours();
 
     // current minutes
     let minutes = date_ob.getMinutes();
-    if(minutes<10){
-        minutes = "0"+minutes
+    if (minutes < 10) {
+        minutes = "0" + minutes
     }
-    if(hours>=13){
-        hours=hours-12
+    if (hours >= 13) {
+        hours = hours - 12
     }
     // prints date & time in YYYY-MM-DD format
-    var rawDate = ( month +"-" + date+ "-"+ year);
+    var rawDate = (month + "-" + date + "-" + year);
     let post = {
         user: req.body.username,
         title: req.body.title,
         message: req.body.message,
-        date:rawDate,
-        time: (hours+":"+minutes),
+        date: rawDate,
+        time: (hours + ":" + minutes),
         postId: await (await posts.countDocuments()).valueOf()
     }
     const insertResult = await posts.insertOne(post);
     client.close();
     // CHANGE THIS REDIRECT TO SOMEWHERE ELSE
-    res.redirect('/feed/'+post.user);
+    res.redirect('/feed/' + post.user);
 };
 exports.editUserView = async (req, res) => {
     await client.connect();
-    const user = await users.findOne({username: req.params.user});
-    res.render('settings',{
-        user:user
+    const user = await users.findOne({ username: req.params.user });
+    res.render('settings', {
+        user: user
     })
 }
 exports.updateUser = async (req, res) => {
     await client.connect();
     // re-salting/hashing new password
 
-    if (req.body.password == ''){
+    if (req.body.password == '') {
         const findResult = await users.find({ username: req.params.user }).toArray();
         const updateResult = await users.updateOne(
             //user id
-            {username:req.params.user},
+            { username: req.params.user },
             //user info 
-            { $set: {
-                password: findResult[0].password,
-                displayName: req.body.displayName,
-                message: req.body.message
-            }}
+            {
+                $set: {
+                    password: findResult[0].password,
+                    displayName: req.body.displayName,
+                    message: req.body.message
+                }
+            }
         )
     } else {
         let salt = bcrypt.genSaltSync(10);
         let hash = bcrypt.hashSync(req.body.password, salt);
         const updateResult = await users.updateOne(
             //user id
-            {username:req.params.user},
+            { username: req.params.user },
             //user info 
-            { $set: {
-                password: hash,
-                displayName: req.body.displayName,
-                message: req.body.message
-            }}
+            {
+                $set: {
+                    password: hash,
+                    displayName: req.body.displayName,
+                    message: req.body.message
+                }
+            }
         )
     }
     client.close();
-    res.redirect('/user/'+req.params.user);
+    res.redirect('/user/' + req.params.user);
 };
 exports.editPostView = async (req, res) => {
     await client.connect();
     var x = Math.floor(req.params.postId);
-    const user = await users.findOne({username: req.params.user});
-    const result = await posts.findOne({postId:x})
-    res.render('editPost',{
-        post:result,
-        user:user
+    const user = await users.findOne({ username: req.params.user });
+    const result = await posts.findOne({ postId: x })
+    res.render('editPost', {
+        post: result,
+        user: user
     })
 }
 exports.editPost = async (req, res) => {
@@ -239,59 +243,61 @@ exports.editPost = async (req, res) => {
     var x = Math.floor(req.body.postId);
     const updateResult = await posts.updateOne(
         //post id instead
-        {postId:x},
+        { postId: x },
         //title and message 
-        { $set: {
-            title: req.body.title,
-            message: req.body.message
+        {
+            $set: {
+                title: req.body.title,
+                message: req.body.message
+            }
         }
-    }
     )
     client.close();
-    res.redirect('/post/'+x+'/'+req.params.user);
+    res.redirect('/post/' + x + '/' + req.params.user);
 }
 exports.deletePost = async (req, res) => {
     await client.connect();
     var x = Math.floor(req.params.postId);
-    const deleteResult = await posts.deleteOne({postId:x})
-    const filteredDocs = await posts.findOne({postId: i});
+    const deleteResult = await posts.deleteOne({ postId: x })
+    const filteredDocs = await posts.findOne({ postId: i });
     const postsResults = await (await posts.find({}).toArray());
     var size = await (await posts.countDocuments()).valueOf();
-    var i =0;
-    for(i=0;i<postsResults.length;i++){
-        if(postsResults[i].postId!=i){
+    var i = 0;
+    for (i = 0; i < postsResults.length; i++) {
+        if (postsResults[i].postId != i) {
             const updateResult = await posts.updateOne(
                 //post id instead
-                {postId:postsResults[i].postId},
+                { postId: postsResults[i].postId },
                 //title and message 
-                { $set: {
-                    postId:i
-                }
-            })
+                {
+                    $set: {
+                        postId: i
+                    }
+                })
         }
-        
 
-    } 
+
+    }
     client.close();
-    res.redirect('/feed/'+req.params.user);
+    res.redirect('/feed/' + req.params.user);
 };
 exports.searchUser = async (req, res) => {
     await client.connect();
-    const searchResult = await users.find({username:new RegExp('.*' + req.body.entry + '.*')}).toArray();
+    const searchResult = await users.find({ username: new RegExp('.*' + req.body.entry + '.*') }).toArray();
     console.log(req.body.entry)
-    const filteredDocs = await users.findOne({username: req.params.user})
+    const filteredDocs = await users.findOne({ username: req.params.user })
     client.close();
 
-    if(searchResult!=null){
-        res.render('explore',{
+    if (searchResult != null) {
+        res.render('explore', {
             userArray: searchResult,
             user: filteredDocs
         });
     }
     else {
-        res.render('explore',{
-            userArray:[{}],
-            user:filteredDocs
+        res.render('explore', {
+            userArray: [{}],
+            user: filteredDocs
         });
     }
 };
@@ -303,11 +309,11 @@ exports.deleteUser = async (req, res) => {
     res.redirect('/login');
 };
 //explore
-exports.exploreUsers = async(req,res) =>{
+exports.exploreUsers = async (req, res) => {
     await client.connect();
-    var usersResults =  await users.find({}).toArray()
+    var usersResults = await users.find({}).toArray()
     usersResults = usersResults.sort(() => Math.random() - 0.5)
-    const filteredDocs = await users.findOne({username: req.params.mainUser})
+    const filteredDocs = await users.findOne({ username: req.params.mainUser })
     res.render('explore', {
         userArray: usersResults,
         user: filteredDocs
@@ -315,30 +321,30 @@ exports.exploreUsers = async(req,res) =>{
 }
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-  }
-exports.joke = async(req, res) => {
+}
+exports.joke = async (req, res) => {
     await client.connect();
     const url = 'https://damp-beach-41286.herokuapp.com/api?category=Computer+Joke&number=1';
     const url1 = 'https://damp-beach-41286.herokuapp.com/api?category=Bad+Pun&number=1';
     const url2 = 'https://damp-beach-41286.herokuapp.com/api?category=Yo+Mama+Joke&number=1';
-    const arry=[url,url1,url2];
-    const filteredDocs = await users.findOne({username: req.params.user});
+    const arry = [url, url1, url2];
+    const filteredDocs = await users.findOne({ username: req.params.user });
     const request = https.request(arry[getRandomInt(3)], (response) => {
         let data = '';
         response.on('data', (chunk) => {
             data = data + chunk.toString();
         });
         response.on('end', () => {
-            let joke = JSON.parse(data); 
+            let joke = JSON.parse(data);
             console.log()
             res.render('joke', {
-                user:filteredDocs,
-                joke :joke[0],
-                
+                user: filteredDocs,
+                joke: joke[0],
+
             });
-          
+
         });
     })
-    request.end() 
-     
+    request.end()
+
 }
